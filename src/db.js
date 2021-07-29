01d83113d085af2ad7e3232e9df23e2cc4b3ba87
Ashar Farooq <ashar744@gmail.com>
2021-07-28 23:03:10 -0400

    firebase integration

--- src/db.js
+++ src/db.js
@@ -1,14 +1,14 @@
-// import firebase from "firebase";
+import firebase from "firebase";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDglcbBXyIfcEAV6rYCwUQr0Ug9YpVbdHA",
-//     authDomain: "meetings-32321.firebaseapp.com",
-//     projectId: "meetings-32321",
-//     storageBucket: "meetings-32321.appspot.com",
-//     messagingSenderId: "416122374807",
-//     appId: "1:416122374807:web:429a1a9225350b3a1f77d2"
-//   };
+const firebaseConfig = {
+    apiKey: "AIzaSyDglcbBXyIfcEAV6rYCwUQr0Ug9YpVbdHA",
+    authDomain: "meetings-32321.firebaseapp.com",
+    projectId: "meetings-32321",
+    storageBucket: "meetings-32321.appspot.com",
+    messagingSenderId: "416122374807",
+    appId: "1:416122374807:web:429a1a9225350b3a1f77d2"
+  };
 
-//   // Initialize Firebase
-//   const firebaseApp = firebase.initializeApp(firebaseConfig);
-// export default firebaseApp.firestore();
+  // Initialize Firebase
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp.firestore();
