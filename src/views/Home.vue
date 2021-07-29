929c52b7ae22e325e80078a8b56290433f0c4b89
Ashar Farooq <ashar744@gmail.com>
2021-07-28 21:57:40 -0400

    cleaned up home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -17,22 +17,26 @@
     <div class="container text-center">
       <div class="row justify-content-center">
         <div class="col-10 col-md-10 col-lg-8 col-xl-7">
-          <h4 class="display-4 text-primary mb-2">Meeting Log</h4>
+          <h4 class="display-4 main">Meeting Log</h4>
           <p class="lead">
             This simple app creates meetings and allows people to check in.
           </p>
 
           <div class="frontbuttons">
+          <div class = "eachbutton">
           <router-link
             class="btn btn-outline-primary mr-2"
             to="/register"
             v-if="!user"
           >Register</router-link>
+          </div>
+          <div class = "eachbutton">
           <router-link
             class="btn btn-outline-primary mr-2"
             to="/login"
             v-if="!user"
           >Log In</router-link>
+          </div>
           </div>
 
           <router-link
@@ -48,17 +52,32 @@
 
 <script>
 export default {
-  name: "home",
+  name: "Home",
   props: ["user"]
 };
 </script>
 
 <style>
 
+@keyframes mainTitle {
+  from {color: red;}
+  to {color: white;}
+  to {color: blue;}
+}
+
+.main {
+  animation-name: mainTitle;
+  animation-duration: 3s;
+  animation-iteration-count: infinite;
+}
+
 .frontbuttons  {
   display: flex;
   justify-content: center;
 }
 
+.eachbutton  {
+  padding-right: 19px;
+}
 </style>
 
